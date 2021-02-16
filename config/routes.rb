bd4388dbb44545e3989a56f41f0d2ed85ee9b895
Brian Firestone <bfirestone2339@gmail.com>
2021-02-16 18:01:06 -0500

    commit tree 2436e7de3379eabeafc9bc66054411466ec7b601

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/students", to: "students#index"
-
-  get "/students/:id", to: "students#show"
+  resources :students, only: [:show, :index]
 end
